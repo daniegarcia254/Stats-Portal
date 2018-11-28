83fb8c0b0e288bc3538e9f267cd57ddab1a5f6aa
daniegarcia254 <daniegarcia254@gmail.com>
2018-11-29 00:34:53 +0100

    Delte docker dependencies

--- client/replace_server_uri.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-output="$(egrep -o 'https:\/\/.*\/production' < $1)"
-
-output_escaped="$(echo "$output" | perl -pe 's/\//\\\//g')"
-
-sed -i "s/API_GATEWAY_URL/$output_escaped/g" app/config/Runtime.js
